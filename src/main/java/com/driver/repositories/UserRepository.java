13c5712747f33370fb49c8734c560f5758c2f9c5
lohit4803 <lohit4803@gmail.com>
2023-01-16 22:43:56 +0530

    loh

--- src/main/java/com/driver/repositories/UserRepository.java
+++ src/main/java/com/driver/repositories/UserRepository.java
@@ -4,13 +4,7 @@ import com.driver.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     public User findByUsername(String username);
-
-    Optional<User> findById(Integer userId);
-
-    Object save(User currentUser);
 }
